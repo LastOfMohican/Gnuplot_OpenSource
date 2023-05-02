590c747621c0d189990d2cb543a3648c2b1165fc
admin <admin@arch.com>
2023-05-02 19:20:08 +0200

    2

---
+++ StrategyMatrix.py
@@ -0,0 +1,11 @@
+class StrategyMatrix:
+    def __init__(self,states,strategies,kDstrat,kCstrat,kDCstrat,cdactions):
+        self.States=states
+        self.Strategies=strategies
+        self.KDstrat=kDstrat
+        self.KCstrat=kCstrat
+        self.KDCstrat=kDCstrat
+        self.CDActions=cdactions
+
+
+
